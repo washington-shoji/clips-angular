48534ba08ca4982e96a5836e1d135949c5adbfc0
washington-shoji <shoji.washington@gmail.com>
2022-04-17 15:20:27 +1000

    refactor(component): Refactored the nav component, for authentication modal service

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-nav',
@@ -7,7 +9,11 @@ import { ModalService } from '../services/modal.service';
   styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-  constructor(public modal: ModalService) {}
+  constructor(
+    public modal: ModalService,
+    public auth: AuthService,
+    private afAuth: AngularFireAuth
+  ) {}
 
   ngOnInit(): void {}
 
@@ -15,5 +21,10 @@ export class NavComponent implements OnInit {
     $event.preventDefault();
 
     this.modal.toggleModal('auth');
+  }
+
+  async logout($event: Event) {
+    $event.preventDefault();
+    await this.afAuth.signOut();
   }
 }
