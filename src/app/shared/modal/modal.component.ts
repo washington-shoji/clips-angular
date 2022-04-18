ed7b761d154fe589f06d40ee56273633068f32bb
washington-shoji <shoji.washington@gmail.com>
2022-04-18 18:28:24 +1000

    refactor(component): Refactored mondal, adding ngOnDestroy to remove element

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ElementRef } from '@angular/core';
+import { Component, Input, OnInit, ElementRef, OnDestroy } from '@angular/core';
 import { ModalService } from './../../services/modal.service';
 
 @Component({
@@ -6,13 +6,17 @@ import { ModalService } from './../../services/modal.service';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy {
   @Input() modalID = '';
 
   constructor(public modal: ModalService, public el: ElementRef) {}
 
   ngOnInit(): void {
     document.body.appendChild(this.el.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    document.body.removeChild(this.el.nativeElement);
   }
 
   closeModal(): void {
